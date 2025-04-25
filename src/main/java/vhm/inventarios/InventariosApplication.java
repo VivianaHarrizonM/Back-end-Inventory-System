78f0d541760c82efeed4b588ec518d7e4a6d5824
Viviana Harrizon <viviana.harrizon@gmail.com>
2025-04-25 14:09:09 -0600

    Edit acces

--- src/main/java/vhm/inventarios/InventariosApplication.java
+++ src/main/java/vhm/inventarios/InventariosApplication.java
@@ -1,9 +1,10 @@
 package vhm.inventarios;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import vhm.inventarios.model.Producto;
-
 
 @SpringBootApplication
 public class InventariosApplication {
@@ -11,14 +12,25 @@ public class InventariosApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InventariosApplication.class, args);
 
-		//Prueba Lombok
+		// Prueba Lombok
 		Producto producto = new Producto();
 		producto.setDescripcion("Camisa M");
 		producto.setPrecio(600.0);
 		producto.setExistencia(30);
 
-		//Imprimir el objeto se llama toString de Lombok
 		System.out.println(producto);
+	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+								.allowedOrigins("https://effervescent-fudge-f6fd66.netlify.app")
+								.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+								.allowedHeaders("*");
+			}
+		};
 	}
 }
